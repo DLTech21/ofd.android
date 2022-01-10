111335b6bca92f99bca900f7a5a9a212c4f8a245
tyz2019 <donal.tong@gmail.com>
2022-01-10 14:59:48 +0800

    jni implement

--- app/src/main/java/io/github/dltech21/ofdrw_aar/MainActivity.java
+++ app/src/main/java/io/github/dltech21/ofdrw_aar/MainActivity.java
@@ -1,33 +1,33 @@
 package io.github.dltech21.ofdrw_aar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.blankj.utilcode.util.FileIOUtils;
 import com.blankj.utilcode.util.PathUtils;
 import com.blankj.utilcode.util.ResourceUtils;
-import com.github.barteksc.pdfviewer.PDFView;
-
 import com.yanzhenjie.permission.Action;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.Permission;
 
-
 import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import io.github.dltech21.ofd.PdfConverter;
+import io.github.dltech21.OFD_Native;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        System.setProperty("org.xml.sax.driver", "org.xmlpull.v1.sax2.Driver");
         setContentView(R.layout.activity_main);
-        PDFView pdfView = findViewById(R.id.pdfView);
         findViewById(R.id.bt_choose).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,13 +40,45 @@ public class MainActivity extends AppCompatActivity {
                                 File file0 = null;
                                 String dirPath = PathUtils.getCachePathExternalFirst() + System.getProperty("file.separator") + "test";
                                 boolean ret = ResourceUtils.copyFileFromAssets("test", dirPath);
-                                Log.e("copy asset", ret+"");
-                                file0 = new File(dirPath, "z.ofd");
-                                Log.e("file", file0.exists()+"");
+                                Log.e("copy asset", ret + "");
 
-                                byte[] pdfByte = PdfConverter.toPdf(file0.getAbsolutePath(), MainActivity.this);
-                                pdfView.fromBytes(pdfByte).load();
+                                String fontPath = PathUtils.getCachePathExternalFirst() + System.getProperty("file.separator") + "fonts";
+                                ResourceUtils.copyFileFromAssets("fonts", fontPath);
+                                Log.e("copy asset", ret + "" + fontPath);
 
+                                file0 = new File(dirPath, "1.ofd");
+                                Log.e("file", file0.exists() + "");
+                                byte[] a = FileIOUtils.readFile2BytesByStream(file0);
+                                ByteBuffer buffer = OFD_Native.newBuffer(a.length);
+                                buffer.order(ByteOrder.nativeOrder());
+                                buffer.limit(buffer.position() + a.length);
+                                OFD_Native.fillBuffer(a, buffer, a.length);
+                                Map<String, String> fontMap = new HashMap<>();
+                                fontMap.put("宋体", fontPath + "/simsun.ttf");
+                                fontMap.put("simsun", fontPath + "/simsun.ttf");
+                                fontMap.put("楷体", fontPath + "/simkai.ttf");
+                                fontMap.put("kaiti", fontPath + "/simkai.ttf");
+                                fontMap.put("kaiti_gb2312", fontPath + "/simkai.ttf");
+                                fontMap.put("楷体_gb2312", fontPath + "/simhei.ttf");
+                                fontMap.put("courier new", fontPath + "/cour.ttf");
+                                fontMap.put("仿宋", fontPath + "/SIMFANG.TTF");
+                                fontMap.put("仿宋_gb2312", fontPath + "/SIMFANG.TTF");
+                                fontMap.put("小标宋体", fontPath + "/方正小标宋简体.ttf");
+                                fontMap.put("方正小标宋简体", fontPath + "/方正小标宋简体.ttf");
+                                fontMap.put("latha", fontPath + "/Latha.ttf");
+                                fontMap.put("tahoma", fontPath + "/Tahoma.ttf");
+                                fontMap.put("times new roman", fontPath + "/times.ttf");
+                                fontMap.put("timesnewromanpsmt", fontPath + "/times.ttf");
+                                fontMap.put("arial", fontPath + "/arial.ttf");
+                                fontMap.put("calibri", fontPath + "/calibri.ttf");
+                                fontMap.put("symbol", fontPath + "/symbol.ttf");
+                                fontMap.put("stcaiyun", fontPath + "/STCAIYUN.TTF");
+                                fontMap.put("mongolian baiti", fontPath + "/monbaiti.ttf");
+                                fontMap.put("wingdings", fontPath + "/wingding.ttf");
+                                long ofdPtr = OFD_Native.readOFD(buffer, buffer.remaining());
+                                int pageIndex = 0;
+                                OFD_Native.drawPage(ofdPtr, pageIndex, dirPath, fontMap);
+                                Log.e("file", "ofd index " + pageIndex + " save in " + dirPath);
                             }
                         })
                         .start();
